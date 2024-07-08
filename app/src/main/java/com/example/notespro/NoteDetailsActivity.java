b6bb73ec1ed85bcba8f6d6c5f73775eb63903c98
MosheShmulevich <moshesh11@ac.sce.ac.il>
2024-07-08 09:15:53 +0300

    Add new note screen design
    new design for not screen

---
+++ app/src/main/java/com/example/notespro/NoteDetailsActivity.java
@@ -0,0 +1,20 @@
+package com.example.notespro;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_note_details);
+
+    }
+}
